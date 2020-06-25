209d1b17c26b14d25dbaa3606c4933411e549b0f
Aleksandr <aleksandros-777@mail.ru>
2020-06-25 18:05:46 +0300

    changes in oop_calc and lunch_dinner

--- OOP_calc.py
+++ OOP_calc.py
@@ -1,6 +1,17 @@
+OPERS = {
+    '+': '__add__',
+    '-': '__sub__',
+    '*': '__mul__',
+    '/': '__truediv__',
+}
+
+
 class Calculator:
     def __init__(self, first):
         self.first = first
+
+    def turn_to_int(self):
+        self.first = int(self.first)
 
     def __add__(self, other):
         res = self.first + other
@@ -17,3 +28,19 @@ class Calculator:
     def __truediv__(self, other):
         res = self.first / other
         return res
+
+
+def main():
+    while True:
+        calc = Calculator(input('Enter first number: > '))
+        if calc.first == '':
+            break
+        calc.turn_to_int()
+        oper_str = input('Enter operator: > ')
+        second_num = int(input('Enter second number: > '))
+        oper = getattr(calc, OPERS.get(oper_str))
+        print(f'\nThe answer is: {oper(second_num)}\n')
+
+
+if __name__ == '__main__':
+    main()
