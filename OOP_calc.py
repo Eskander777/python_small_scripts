ca80bd1d4def29fbde9cec8d8a4ca563f4301c9b
Aleksandr <aleksandros-777@mail.ru>
2020-06-26 13:38:34 +0300

    changed oop_calc.py

--- OOP_calc.py
+++ OOP_calc.py
@@ -4,6 +4,10 @@ class Calculator:
         '-': '__sub__',
         '*': '__mul__',
         '/': '__truediv__',
+        '**': '__pow__',
+        '//': '__floordiv__',
+        '%': '__mod__'
+
     }
 
     def __init__(self, first):
@@ -25,24 +29,38 @@ class Calculator:
         res = self.first / other
         return res
 
+    def __pow__(self, other):
+        res = self.first ** other
+        return res
+
+    def __floordiv__(self, other):
+        res = self.first // other
+        return res
+
+    def __mod__(self, other):
+        res = self.first % other
+        return res
+
     def turn_to_int(self):
         self.first = int(self.first)
 
     def make_calculations(self):
-        while True:
-            try:
-                self.turn_to_int()
-                oper_str = input('Enter operator: > ')
-                if oper_str not in self.opers.keys():
-                    raise ValueError
-                second_num = int(input('Enter second number: > '))
-                oper = getattr(self, self.opers.get(oper_str))
-            except (TypeError, ValueError):
-                print('\nWrong input. Try again!\n')
-                raise
-            else:
-                print(f'\nThe answer is: {oper(second_num)}\n')
-                break
+        try:
+            self.turn_to_int()
+            oper_str = input('Enter operator: > ')
+            if oper_str not in self.opers.keys():
+                raise ValueError
+            second_num = int(input('Enter second number: > '))
+            oper = getattr(self, self.opers.get(oper_str))
+            result = oper(second_num)
+        except ZeroDivisionError:
+            print("\nYou can't divide by zero. Try again!\n")
+        except (TypeError, ValueError):
+            print('\nWrong input. Try again!\n')
+            raise
+        else:
+            print(f'Operation: {self.first} {oper_str} {second_num}')
+            print(f'\nThe answer is: {result}\n')
 
 
 def main():
