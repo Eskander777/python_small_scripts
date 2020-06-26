7345a9e584d1886bba3073db94526a10ad90e7c4
Aleksandr <aleksandros-777@mail.ru>
2020-06-26 10:46:31 +0300

    changes in oop_calc.py

--- OOP_calc.py
+++ OOP_calc.py
@@ -1,17 +1,13 @@
-OPERS = {
-    '+': '__add__',
-    '-': '__sub__',
-    '*': '__mul__',
-    '/': '__truediv__',
-}
-
-
 class Calculator:
+    opers = {
+        '+': '__add__',
+        '-': '__sub__',
+        '*': '__mul__',
+        '/': '__truediv__',
+    }
+
     def __init__(self, first):
         self.first = first
-
-    def turn_to_int(self):
-        self.first = int(self.first)
 
     def __add__(self, other):
         res = self.first + other
@@ -29,17 +25,36 @@ class Calculator:
         res = self.first / other
         return res
 
+    def turn_to_int(self):
+        self.first = int(self.first)
+
+    def make_calculations(self):
+        while True:
+            try:
+                self.turn_to_int()
+                oper_str = input('Enter operator: > ')
+                if oper_str not in self.opers.keys():
+                    raise ValueError
+                second_num = int(input('Enter second number: > '))
+                oper = getattr(self, self.opers.get(oper_str))
+            except (TypeError, ValueError):
+                print('\nWrong input. Try again!\n')
+                raise
+            else:
+                print(f'\nThe answer is: {oper(second_num)}\n')
+                break
+
 
 def main():
     while True:
-        calc = Calculator(input('Enter first number: > '))
+        calc = Calculator(
+            input('Enter first number (press "Enter" to exit): > '))
         if calc.first == '':
             break
-        calc.turn_to_int()
-        oper_str = input('Enter operator: > ')
-        second_num = int(input('Enter second number: > '))
-        oper = getattr(calc, OPERS.get(oper_str))
-        print(f'\nThe answer is: {oper(second_num)}\n')
+        try:
+            calc.make_calculations()
+        except ValueError:
+            continue
 
 
 if __name__ == '__main__':
